9355cc8da3a4baca0881e4e456b9029e60d68602
Andrew Austin <ndrwstn@users.noreply.github.com>
2026-09-13 11:50:52 -0400

    Remove superseded ICC Test generator

--- scripts/generate-icc-test.mjs
+++
@@ -1,5 +0,0 @@
-import { mkdir, writeFile } from "node:fs/promises";
-import { buildIccTestCalendar } from "../src/icc-test.mjs";
-
-await mkdir("public", { recursive: true });
-await writeFile("public/icc-test.ics", await buildIccTestCalendar(process.env.CRICKETDATA_API_KEY), "utf8");
